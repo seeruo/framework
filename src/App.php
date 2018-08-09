56f00c7f5b1c7ca99122e8f34857c2b6cca8d630
seeruo <cdking95@qq.com>
2018-08-09 15:36:49 +0800

    config edit

--- src/App.php
+++ src/App.php
@@ -14,18 +14,22 @@ class App
      * 初始配置
      */
 	private $config = [
+        // 基础设置::必填
         'root'              => '',                  // 应用根路径
+        // 以下配置根据情况选填
         'title'             => '我的博客',          // 网站名称
         'url'               => '',                  // 网站URL
         'themes'            => 'default',           // 网站主题
         'author'            => 'danier',            // 您的名字
+        'home_page'         => '',                  // 主页文件
+        // 本地调试设置
         'server_address'    => 'localhost:9001',    // 本地服务器调试地址
         'auto_open'         => false,               // 自动打开浏览器
+        // 发布到服务器空间
         'push_type'         => 'ssh',               // 推送到服务器的方式 ssh/git
         'push_user'         => 'root',              // SSH账号
         'push_address'      => '127.0.0.1',         // SSH推送地址
         'push_path'         => '/var/www/html/blog',// SSH推送路径,该服务器的路径必须要开启写权限
-        'home_page'         => '',           // 主页
     ];
 
     /**
